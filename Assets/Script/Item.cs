815ae4a4f380c8147721d0231715399aed04bf82
ChongSiuHin <107177861+ChongSiuHin@users.noreply.github.com>
2024-01-27 12:33:08 +0800

    14
    itemScript small change

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public ItemType itemType;
+
+    public Sprite sprite;
+    private SpriteRenderer spriteRenderer;
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public void Interact()
     {
-        Debug.Log("u was click E");
-        CalculationSystem.UpdateItemStatus(itemType, true);
-        Destroy(gameObject);
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+        else
+        {
+            Debug.Log("u was click E");
+            CalculationSystem.UpdateItemStatus(itemType, true);
+            Destroy(gameObject);
+        }
     }
+
+
 }
