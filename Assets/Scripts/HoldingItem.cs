b1cb0b2583a4389646f5c9b1210bfb05f5d545ae
owokamii <us3r424815@gmail.com>
2024-01-27 21:49:05 +0800

    30
    fridge animation but got problem

--- Assets/Scripts/HoldingItem.cs
+++ Assets/Scripts/HoldingItem.cs
@@ -6,42 +6,62 @@ public class HoldingItem : MonoBehaviour
     public HealthBar healthBar;
     public bool isCurrentlyInteracting = false;
     public Sprite sprite;
+    
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
     public CanvasGroup healthBarCanvasGroup;
+    private bool HealbarWasFull;
+    public HealthBar healthbar;
 
     private void Start()
     {
         GameObject healthBarObj = GameObject.FindGameObjectWithTag("HealthBar");
+
         if (healthBarObj != null)
         {
             healthBarCanvasGroup = healthBarObj.GetComponent<CanvasGroup>();
         }
-            spriteRenderer = GetComponent<SpriteRenderer>();
+        HealbarWasFull = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (isCurrentlyInteracting && healthBar.IsFull() & sprite != null)
+        if (isCurrentlyInteracting && healthBar.IsFull() & sprite != null && !HealbarWasFull)
         {
            ChangeSprite();
+            animator.SetBool("Interacting", true);
             healthBarCanvasGroup.alpha = 0;
+            HealbarWasFull = true;
+
         }
-        else if (isCurrentlyInteracting && healthBar.IsFull())
+        else if (isCurrentlyInteracting && healthBar.IsFull() && sprite == null)
         {
             healthBarCanvasGroup.alpha = 0;
             CalculateEnding.UpdateItemStatus(ItemType, true);
             Destroy(gameObject);
         }
+        else if (isCurrentlyInteracting && !HealbarWasFull && healthBar.Click == false)
+        {
+            animator.SetBool("Interacting", false);
+        }
     }
 
     public void StartInteraction()
     {
+        animator.SetBool("Interacting", true);
         isCurrentlyInteracting = true;
         healthBar.Click = true;
     }
 
     public void StopInteraction()
     {
+        //if(healthBar.slider.value != 0)
+        //{
+        //    animator.SetBool("Interacting", false);
+        //}
         isCurrentlyInteracting = false;
         healthBar.Click = false;
     }
