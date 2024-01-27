c4833dd911fea440f47a8463beec24802306460d
ChongSiuHin <107177861+ChongSiuHin@users.noreply.github.com>
2024-01-28 05:46:30 +0800

    51
    bug fix selection and change sprite make some changes in item3 script

--- Assets/Scripts/Item3.cs
+++ Assets/Scripts/Item3.cs
@@ -6,13 +6,13 @@ public class Item3 : MonoBehaviour
 
     private SpriteRenderer spritePlayer;
     public Sprite PlayerNewSprite;
-    private SpriteRenderer spriteOri;
+    public SpriteRenderer spriteOri;
     public Sprite original;
     // Start is called before the first frame update
     void Start()
     {
         question = FindObjectOfType<SelectionQuestion>();
-        spriteOri = FindObjectOfType<SpriteRenderer>();
+        //spriteOri = FindObjectOfType<SpriteRenderer>();
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
         {
@@ -31,8 +31,8 @@ public class Item3 : MonoBehaviour
         if (selection)
         {
             Debug.Log("Yes");
-            spritePlayer.sprite = PlayerNewSprite;
             spriteOri.sprite = original;
+            spritePlayer.sprite = PlayerNewSprite;
         }
         else
         {
