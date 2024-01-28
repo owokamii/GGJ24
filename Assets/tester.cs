cfe59bf10ee4f35c44f82bd4a011fab88177155a
owokamii <us3r424815@gmail.com>
2024-01-28 12:27:54 +0800

    61
    idk

---
+++ Assets/tester.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class tester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+}
